3ad9cd28c3b81f4ae17a61860b9b389e35ac010c
Sebastian Kostecki <sebastian.ko@modulesgarden.com>
2023-02-19 23:26:33 +0100

    Extracted sidebar to own component

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-menu contentId="main-content">
+    <sidebar-content></sidebar-content>
+  </ion-menu>
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <slot></slot>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import SidebarContent from "./SidebarContent.vue";
+export default {
+  name: "BaseLayout",
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonContent,
+    SidebarContent,
+  },
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+ion-toolbar {
+  --background: transparent no-repeat fixed center;
+  --color: white;
+  font-style: bold;
+  position: absolute;
+  top: 0;
+}
+</style>
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,90 +1,54 @@
 <template>
-  <ion-page>
-    <menu-app></menu-app>
-    <div class="ion-page" id="main-content">
-      <ion-header>
-        <ion-toolbar>
-          <ion-buttons slot="start">
-            <ion-menu-button></ion-menu-button>
-          </ion-buttons>
-          <ion-title>Ustawienia</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <ion-list lines="none">
-          <ion-item class="ion-margin-horizontal">
-            <ion-label position="stacked">Nazwa róży</ion-label>
-            <ion-input placeholder="Wpisz nazwę róży"></ion-input>
-          </ion-item>
-          <ion-item class="ion-margin-horizontal">
-            <ion-label position="stacked">Początek modlitwy</ion-label>
-            <ion-input type="date"></ion-input>
-          </ion-item>
-          <ion-item class="ion-margin-horizontal">
-            <ion-label position="stacked">Pierwsza tajemnica</ion-label>
-            <ion-select
-              interface="popover"
-              placeholder="Wybierz tajemnicę różańca"
-            >
-              <ion-select-option value="apples">Apples</ion-select-option>
-              <ion-select-option value="oranges">Oranges</ion-select-option>
-              <ion-select-option value="bananas">Bananas</ion-select-option>
-              <ion-select-option value="apples">Apples</ion-select-option>
-              <ion-select-option value="oranges">Oranges</ion-select-option>
-              <ion-select-option value="bananas">Bananas</ion-select-option>
-              <ion-select-option value="apples">Apples</ion-select-option>
-              <ion-select-option value="oranges">Oranges</ion-select-option>
-              <ion-select-option value="bananas">Bananas</ion-select-option>
-            </ion-select>
-          </ion-item>
-          <ion-item class="ion-margin-horizontal">
-            <ion-label position="stacked">Płeć</ion-label>
-            <ion-select interface="popover" placeholder="Wybierz płeć">
-              <ion-select-option value="women">Kobieta</ion-select-option>
-              <ion-select-option value="men">Mężczyzna</ion-select-option>
-            </ion-select>
-          </ion-item>
-        </ion-list>
-      </ion-content>
-    </div>
-  </ion-page>
+  <base-layout title="Ustawienia">
+    <ion-list lines="none">
+      <ion-item class="ion-margin-horizontal">
+        <ion-label position="stacked">Nazwa róży</ion-label>
+        <ion-input placeholder="Wpisz nazwę róży"></ion-input>
+      </ion-item>
+      <ion-item class="ion-margin-horizontal">
+        <ion-label position="stacked">Początek modlitwy</ion-label>
+        <ion-input type="date"></ion-input>
+      </ion-item>
+      <ion-item class="ion-margin-horizontal">
+        <ion-label position="stacked">Pierwsza tajemnica</ion-label>
+        <ion-select interface="popover" placeholder="Wybierz tajemnicę różańca">
+          <ion-select-option value="apples">Apples</ion-select-option>
+          <ion-select-option value="oranges">Oranges</ion-select-option>
+          <ion-select-option value="bananas">Bananas</ion-select-option>
+          <ion-select-option value="apples">Apples</ion-select-option>
+          <ion-select-option value="oranges">Oranges</ion-select-option>
+          <ion-select-option value="bananas">Bananas</ion-select-option>
+          <ion-select-option value="apples">Apples</ion-select-option>
+          <ion-select-option value="oranges">Oranges</ion-select-option>
+          <ion-select-option value="bananas">Bananas</ion-select-option>
+        </ion-select>
+      </ion-item>
+      <ion-item class="ion-margin-horizontal">
+        <ion-label position="stacked">Płeć</ion-label>
+        <ion-select interface="popover" placeholder="Wybierz płeć">
+          <ion-select-option value="women">Kobieta</ion-select-option>
+          <ion-select-option value="men">Mężczyzna</ion-select-option>
+        </ion-select>
+      </ion-item>
+    </ion-list>
+  </base-layout>
 </template>
 
 <script>
-import {
-  IonPage,
-  IonHeader,
-  IonTitle,
-  IonContent,
-  IonToolbar,
-  IonButtons,
-  IonList,
-  IonItem,
-  IonInput,
-  IonLabel,
-} from "@ionic/vue";
-import MenuApp from "../components/MenuApp.vue";
+import { IonList, IonItem, IonInput, IonLabel } from "@ionic/vue";
 export default {
   name: "SettingsApp",
   components: {
-    IonPage,
-    IonHeader,
-    IonTitle,
-    IonContent,
-    IonToolbar,
-    IonButtons,
-    MenuApp,
     IonList,
     IonItem,
     IonInput,
-
     IonLabel,
   },
 };
 </script>
 
 <style scoped>
-ion-segment {
-  --background: primary;
+ion-list {
+  margin-top: 64px;
 }
 </style>
