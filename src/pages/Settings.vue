e57f2cd78662103981a7d7bcee90738ebf0e4064
Sebastian Kostecki <sebastian.ko@modulesgarden.com>
2023-02-19 21:04:28 +0100

    Created empty setting page

---
+++ src/pages/Settings.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <menu-app></menu-app>
+    <div class="ion-page" id="main-content">
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-menu-button></ion-menu-button>
+          </ion-buttons>
+          <ion-title>O Nią & O Niego</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content> </ion-content>
+    </div>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonToolbar,
+  IonButtons,
+} from "@ionic/vue";
+import MenuApp from "../components/MenuApp.vue";
+export default {
+  name: "SettingsApp",
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonButtons,
+    MenuApp,
+  },
+};
+</script>
