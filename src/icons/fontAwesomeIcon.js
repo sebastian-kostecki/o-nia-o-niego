becc5e93e660e872671764e6c31a14ca01bf6de7
Sebastian Kostecki <sebastian.ko@modulesgarden.com>
2023-02-22 13:14:48 +0100

    Extracted FontAwesomeIcons to separated file

---
+++ src/icons/fontAwesomeIcon.js
@@ -0,0 +1,25 @@
+/* Import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* Import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* Import specific icons */
+import {
+  faHouse,
+  faGear,
+  faCircleInfo,
+  faBookOpen,
+  faBookBible,
+  faHandsPraying,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faHouse);
+library.add(faGear);
+library.add(faCircleInfo);
+library.add(faBookOpen);
+library.add(faBookBible);
+library.add(faHandsPraying);
+
+export default FontAwesomeIcon
