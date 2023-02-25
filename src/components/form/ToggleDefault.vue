13bd978393919f1d0d7c6c4aaa483042ebb73434
Sebastian Kostecki <sebastian.ko@modulesgarden.com>
2023-02-25 22:15:33 +0100

    Extracted toggle component

---
+++ src/components/form/ToggleDefault.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-label>{{ title }}</ion-label>
+  <ion-toggle
+      slot="end"
+      v-model="selectedValue"
+      @ionChange="setValue"
+  ></ion-toggle>
+</template>
+
+<script>
+import {IonLabel, IonToggle} from "@ionic/vue";
+export default {
+  name: "ToggleDefault",
+  components: {
+    IonToggle,
+    IonLabel
+  },
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      selectedValue: true,
+    }
+  },
+  methods: {
+    setValue() {
+      this.$emit('set-value', this.selectedValue)
+    }
+  }
+}
+</script>
--- src/components/settings/InterfaceList.vue
+++ src/components/settings/InterfaceList.vue
@@ -4,25 +4,41 @@
       <ion-label>Dashboard</ion-label>
     </ion-list-header>
     <ion-item class="ion-margin-horizontal">
-      <ion-label>Rozważania</ion-label>
-      <ion-toggle slot="end"></ion-toggle>
+      <toggle-default
+          title="Rozważania"
+          @set-value="setIsReflectionsView"
+      ></toggle-default>
     </ion-item>
     <ion-item class="ion-margin-horizontal">
-      <ion-label>Ewangelia z dnia</ion-label>
-      <ion-toggle slot="end"></ion-toggle>
+      <toggle-default
+          title="Ewangelia z dnia"
+          @set-value="setIsGospelView"
+      ></toggle-default>
     </ion-item>
   </ion-list>
 </template>
 
 <script>
 import {IonList, IonItem, IonLabel, IonListHeader} from "@ionic/vue";
+import ToggleDefault from "@/components/form/ToggleDefault.vue";
+import DatabaseService from "@/services/database";
+
 export default {
   name: "InterfaceList",
   components: {
     IonList,
     IonItem,
     IonLabel,
-    IonListHeader
+    IonListHeader,
+    ToggleDefault
   },
+  methods: {
+    async setIsReflectionsView(value) {
+      await DatabaseService.setData('is_reflections_view', value);
+    },
+    async setIsGospelView(value) {
+      await DatabaseService.setData('is_gospel_view', value);
+    },
+  }
 }
 </script>
