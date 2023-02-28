e2c0d569206bfa0ef77a2a62303687a8913bf037
Sebastian Kostecki <sebastian.ko@modulesgarden.com>
2023-02-28 22:17:31 +0100

    Changed fetching data to dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,37 +1,36 @@
 <template>
   <base-layout title="O Nią & O Niego">
-    <ion-content>
-      <dashboard-panel></dashboard-panel>
+    <ion-content v-if="!loading">
+      <dashboard-panel
+          :date="dateToDisplay"
+          :liturgicDay="liturgicDayTitle"
+      ></dashboard-panel>
       <dashboard-card
           is-showed="true"
           title="Tajemnica różańca"
-          :subtitle="mysteryOfRosary.name"
-          :content="mysteryOfRosary.text"
+          :subtitle="mystery.text"
+          :content="mystery.text"
           icon="fa-solid fa-cross"
       ></dashboard-card>
       <dashboard-card
           :is-showed="isPatronPrayerView"
           title="Modlitwa do patrona"
-          subtitle="Święta Rodzina"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet in ex
-          doloremque inventore, possimus quam eaque laboriosam aut nulla ipsa non
-          nisi nesciunt doloribus, sapiente est facere porro expedita dolor?"
+          :subtitle="patron.text"
+          :content="patron.prayer"
           icon="fa-solid fa-hands-praying"
       ></dashboard-card>
       <dashboard-card
           :is-showed="isReflectionsView"
           title="Rozważanie"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet in ex
-          doloremque inventore, possimus quam eaque laboriosam aut nulla ipsa non
-          nisi nesciunt doloribus, sapiente est facere porro expedita dolor?"
+          :content="mystery.reflection"
           icon="fa-solid fa-book-open"
       ></dashboard-card>
       <dashboard-card
           :is-showed="isGospelView"
           title="Ewangelia na dziś"
           icon="fa-solid fa-bible"
-          :content="gospel"
-          :subtitle="gospelTitle"
+          :content="gospelText"
+          :subtitle="gospel.title"
           :is-showed-subtitle="false"
       ></dashboard-card>
     </ion-content>
@@ -39,43 +38,45 @@
 </template>
 
 <script>
+import axios from "axios";
 import BaseLayout from "../components/BaseLayout.vue";
 import DashboardCard from "@/components/dashboard/DashboardCard.vue";
 import DashboardPanel from "@/components/dashboard/DashboardPanel.vue";
 import DatabaseService from "@/services/database";
+import SettingsVariablesMixins from "@/mixins/settingsVariablesMixins";
+
+const Api = axios.create();
 
 export default {
   name: "dash-board",
   components: {
     BaseLayout,
     DashboardCard,
-    DashboardPanel
+    DashboardPanel,
   },
+  mixins: [SettingsVariablesMixins],
   data() {
     return {
-      mysteryOfRosary: {
-        name: "Zwiastowanie NMP",
-        text: "Oto ja służebnica Pańska, niech mi się stanie według słowa twego"
-      },
+      gospel: null,
+      dateToDisplay: "",
+      liturgicDayTitle: "",
+      firstMystery: null,
+      mystery: null,
+      patron: null,
+      beginningDate: "",
       timeToEnd: "Do końca pozostało 20 dni",
       isGospelView: true,
       isReflectionsView: true,
-      isPatronPrayerView: true
+      isPatronPrayerView: true,
+      loading: true
     };
   },
   computed: {
-    gospel() {
-      return this.$store.getters.getGospel;
-    },
-    gospelTitle() {
-      return this.$store.getters.getGospelTitle;
-    },
-    date() {
-      return this.$store.getters.getDateDisplayed;
-    },
-    liturgicTitle() {
-      return this.$store.getters.getLiturgicTitle;
-    },
+    gospelText() {
+      let gospelAfter = this.gospel.text.replace(/\[[^\]]*\]/g, '');
+      gospelAfter = gospelAfter.replaceAll(']', '');
+      return gospelAfter;
+    }
   },
   created() {
     DatabaseService.initDatabase();
@@ -85,9 +86,15 @@ export default {
   },
   methods: {
     async getViews() {
+      await this.setLoading(true);
+      await this.getLiturgyOfDay();
       await this.getIsGospelView();
       await this.getIsReflectionsView();
       await this.getIsPatronPrayerView();
+      await this.getMysteryOfRosary();
+      await this.getPatronPrayer();
+      await this.getBeginningDate();
+      await this.setLoading(false)
     },
     async getIsGospelView() {
       this.isGospelView = await DatabaseService.getData('is_gospel_view');
@@ -97,6 +104,55 @@ export default {
     },
     async getIsPatronPrayerView() {
       this.isPatronPrayerView = await DatabaseService.getData('is_patron_prayer_view');
+    },
+    async getMysteryOfRosary() {
+      const now = new Date();
+      const beginning = await this.getBeginningDate();
+      const difference = this.monthsDiff(beginning, now);
+      const mysteryOfRosaryId = await DatabaseService.getData('first_mystery');
+      this.mystery = await this.getMystery(mysteryOfRosaryId + difference + 1);
+    },
+    getMystery(id) {
+      return this.mysteryOfRosary.find(item => item.id === id)
+    },
+    async getPatronPrayer() {
+      const patronId = await DatabaseService.getData('patron');
+      this.patron = await this.getPatron(patronId);
+    },
+    getPatron(id) {
+      return this.patrons.find(item => item.id === id);
+    },
+    async getBeginningDate() {
+      const date = await DatabaseService.getData('beginning_date');
+      return new Date(date);
+    },
+    monthsDiff(date1, date2) {
+      let diff = (date2.getFullYear() - date1.getFullYear()) * 12;
+      diff -= date1.getMonth() + 1;
+      diff += date2.getMonth();
+      return diff;
+    },
+    setLoading(value) {
+      this.loading = value;
+    },
+    getLiturgyOfDay() {
+      Api.get(`https://publication.evangelizo.ws/PL/days/${this.getDate()}`)
+        .then((response) => {
+          let readings = response.data.data.readings;
+          this.gospel = readings.at(-1);
+          this.dateToDisplay = response.data.data.date_displayed;
+          this.liturgicDayTitle = response.data.data.liturgic_title;
+        })
+        .catch(function () {
+          // handle error
+        })
+    },
+    getDate() {
+      let today = new Date();
+      let year = today.getFullYear();
+      let month = (today.getMonth() + 1).toString().padStart(2, '0');
+      let day = today.getDate().toString().padStart(2, '0');
+      return `${year}-${month}-${day}`;
     },
   }
 };
